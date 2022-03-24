2096761efc2cffa06dcecd5032bef502a950d9f6
ayamohsen <ayamohsen272022@gmail.com>
2022-03-24 18:40:14 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(	48.864716, 2.349014),
+    zoom: 8,
+  );
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar( title: Text("Google Maps"),
+      ),
+      body:  GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+     // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
